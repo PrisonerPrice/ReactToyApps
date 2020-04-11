57e22fddee5da1d8cc1649b37869e3ab40e91602
Hangbo <zhzhanghangbo@hotmail.com>
2020-04-10 21:09:19 -0400

    Enhance functionalities and polish ths UI

--- lunch-calculator/src/index.js
+++ lunch-calculator/src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 class Input extends React.Component {
 	constructor(props) {
@@ -14,7 +23,11 @@ class Input extends React.Component {
 			ChenPay: 0,
 			ZhuPay: 0,
 			ZhangPay: 0,
-			sum: 0
+			shareItem: 0,
+			sum: 0,
+			addOnOriginal: 0,
+			adding: 0,
+			addsOnSum: 0,
 		};
 	}
 	
@@ -31,6 +44,18 @@ class Input extends React.Component {
 		if (code === 3) this.setState({
 			ZhangMoney: event.target.value,
 		})
+		if (code === 4) this.setState({
+			shareItem: event.target.value,
+		})
+	}
+
+	updateAddOns(event, code) {
+		if (code === 0) this.setState({
+			addOnOriginal: event.target.value,
+		});
+		if (code === 1) this.setState({
+			adding: event.target.value,
+		});
 	}
 	
 	updateSum(event) {
@@ -45,6 +70,7 @@ class Input extends React.Component {
 		var ChenRatio = 0;
 		var ZhuRatio = 0;
 		var ZhangRatio = 0;
+		var shareIndividual = 0;
 		if(this.state.BianMoney != null) {
 			total = parseFloat(total) + parseFloat(this.state.BianMoney);
 		}
@@ -57,10 +83,14 @@ class Input extends React.Component {
 		if(this.state.ZhangMoney != null) {
 			total = parseFloat(total) + parseFloat(this.state.ZhangMoney);
 		}
-		BianRatio = this.state.BianMoney / total;
-		ChenRatio = this.state.ChenMoney / total;
-		ZhuRatio = this.state.ZhuMoney / total;
-		ZhangRatio = this.state.ZhangMoney / total;
+		if(this.state.shareItem != null) {
+			total = parseFloat(total) + parseFloat(this.state.shareItem);
+			shareIndividual = parseFloat(this.state.shareItem) / 4;
+		}
+		BianRatio = (parseFloat(this.state.BianMoney) + parseFloat(shareIndividual)) / total;
+		ChenRatio = (parseFloat(this.state.ChenMoney) + parseFloat(shareIndividual)) / total;
+		ZhuRatio = (parseFloat(this.state.ZhuMoney) + parseFloat(shareIndividual)) / total;
+		ZhangRatio = (parseFloat(this.state.ZhangMoney) + parseFloat(shareIndividual)) / total;
 		this.setState({
 			BianPay: (BianRatio * this.state.sum).toPrecision(4),
 			ChenPay: (ChenRatio * this.state.sum).toPrecision(4),
@@ -68,28 +98,65 @@ class Input extends React.Component {
 			ZhangPay: (ZhangRatio * this.state.sum).toPrecision(4),
 		});
 	}
+
+	calculateAddsOn() {
+		var newOriginal = parseFloat(this.state.addOnOriginal) + parseFloat(this.state.adding);
+		this.setState({
+			addOnOriginal: newOriginal,
+		})
+	}
 	
 		
 	render() {
 		return (
 			<div>
-				<h1>Bian</h1>
-				<input onChange={(event) => this.updateSingle(event, 0)}/>
-				<h1>Chen</h1>
-				<input onChange={(event) => this.updateSingle(event, 1)}/>
-				<h1>Zhu</h1>
-				<input onChange={(event) => this.updateSingle(event, 2)}/>
-				<h1>Zhang</h1>
-				<input onChange={(event) => this.updateSingle(event, 3)}/>
-				<h1>Total</h1>
-				<input onChange={(event) => this.updateSum(event)}/><br/>
-				
-				<button onClick={() => this.calculate()}>Calculate</button>
-				
-				<h2>Bian should pay {this.state.BianPay} dollars</h2>
-				<h2>Chen should pay {this.state.ChenPay} dollars</h2>
-				<h2>Zhu should pay {this.state.ZhuPay} dollars</h2>
-				<h2>Zhang should pay {this.state.ZhangPay} dollars</h2>
+			<Container fluid className="containerStyle">
+			<Row><h2>Lunch Calculator</h2></Row>
+			<Row>
+				<Col xs={4}>
+					<Row>
+						<Col xs={5}><h3>Bian</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSingle(event, 0)}/></Col>
+					</Row>
+					<Row className="rowMargin">
+						<Col xs={5}><h3>Chen</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSingle(event, 1)}/></Col>
+					</Row>
+					<Row className="rowMargin">
+						<Col xs={5}><h3>Zhu</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSingle(event, 2)}/></Col>
+					</Row>
+					<Row className="rowMargin">
+						<Col xs={5}><h3>Zhang</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSingle(event, 3)}/></Col>
+					</Row>
+					<Row className="rowMargin">
+						<Col xs={5}><h3>Shared</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSingle(event, 4)}/></Col>
+					</Row>
+					<Row className="rowMargin">
+						<Col xs={5}><h3>Total</h3></Col>
+						<Col xs={4}><input onChange={(event) => this.updateSum(event)}/><br/></Col>
+					</Row>
+					<Button className="btnMargin" onClick={() => this.calculate()}>Calculate</Button>
+				</Col>
+				<Col xs={4}>
+					<h3>Bian should pay {this.state.BianPay} dollars</h3>
+					<h3 className="rowMargin">Chen should pay {this.state.ChenPay} dollars</h3>
+					<h3 className="rowMargin">Zhu should pay {this.state.ZhuPay} dollars</h3>
+					<h3 className="rowMargin">Zhang should pay {this.state.ZhangPay} dollars</h3>
+				</Col>
+				<Col xs={4}>
+					<h3>Adds-On Calculator</h3>
+					<h3>Before:</h3>
+					<input onChange={(event) => this.updateAddOns(event, 0)}/>
+					<h3 className="rowMargin">Adds-On:</h3>
+					<input onChange={(event) => this.updateAddOns(event, 1)}/><br/>
+					<Button className="btnMargin2" onClick={() => this.calculateAddsOn()}>Calculate</Button>
+					<h3>Adds-On total is: {this.state.addOnOriginal} dollars</h3>
+				</Col>
+			</Row>
+			</Container>
 			</div>
 		);
 	}
